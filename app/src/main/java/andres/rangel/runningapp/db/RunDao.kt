61dfb72b447afb642d206e15c42a67572846bf62
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-04-17 23:48:28 -0500

    Added model class Run and Dao interface RunDao

---
+++ app/src/main/java/andres/rangel/runningapp/db/RunDao.kt
@@ -0,0 +1,42 @@
+package andres.rangel.runningapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY averageSpeedInKmH DESC")
+    fun getAllRunsSortedByAverageSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTime(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT AVG(averageSpeedInKmH) FROM running_table")
+    fun getAverageSpeed(): LiveData<Float>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTime(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+}
